95301338bce0d20e6491e008060998e3643e9dee
nagarajankola <ankola.nagraj07@gmail.com>
2021-06-28 17:50:20 +0530

    added addtocart rote

--- index.js
+++ index.js
@@ -20,6 +20,30 @@ app.use(passport.initialize());
 require("./middlewares/passport")(passport);
 
 //User Router Middlewares
+app.post("/api/users/addToCart", async (req, res) => {
+  try {
+    console.log(req.body);
+    console.log(req.body.params.productID);
+    const response = await Product.findOneAndUpdate(
+      {
+        _id: req.body.params.userID,
+      },
+      {
+        $addToSet: {
+          cartItems: req.body.params.productID,
+        },
+      }
+    );
+    // console.log(response);
+    res.send("ok");
+  } catch (error) {
+    console.log(error);
+    res.status(404).send({
+      message:"fail to add"
+    })
+  }
+
+});
 app.use("/api/users", require("./routes/users"));
 app.use("/api/v1/products", productRouter);
 
